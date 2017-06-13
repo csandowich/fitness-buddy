07910aaaa23850bc7cebb7558647bf3151938b38
Caleb Sandowich <calebsandowich@calebs-mbp.attlocal.net>
2017-06-13 01:13:11 -0700

    added firebase database to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,10 @@
-alert("h")
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXN9XhGk6V7nODMRaaccn1_e-NOvnGtdY",
+    authDomain: "fitness-buddy-d10a3.firebaseapp.com",
+    databaseURL: "https://fitness-buddy-d10a3.firebaseio.com",
+    projectId: "fitness-buddy-d10a3",
+    storageBucket: "fitness-buddy-d10a3.appspot.com",
+    messagingSenderId: "556464695807"
+  };
+  firebase.initializeApp(config);
