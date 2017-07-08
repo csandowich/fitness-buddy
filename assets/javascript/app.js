99c567c767699dd5b204d1512206052b084ccf7d
Thanita Emmy Jarzebinski <emmyjarz@gmail.com>
2017-07-07 22:35:39 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,6 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBu4pjbzzKjgREDEnQK3sYa4_0TUQDnVcc",
-  authDomain: "test-b14d0.firebaseapp.com",
-  databaseURL: "https://test-b14d0.firebaseio.com",
-  projectId: "test-b14d0",
-  storageBucket: "test-b14d0.appspot.com",
-  messagingSenderId: "781144526712"
+please put in your configuration
 };
 firebase.initializeApp(config);
 
